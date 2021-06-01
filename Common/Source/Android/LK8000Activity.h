f7f08d22fcf43f84897eadcef8d0596d142aab6c
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2021-06-02 01:32:29 +0200

    Android : Load external task file

--- Common/Source/Android/LK8000Activity.h
+++ Common/Source/Android/LK8000Activity.h
@@ -18,6 +18,7 @@ class LK8000Activity {
   static Java::TrivialClass cls;
   static jmethodID check_permissions_method;
   static jmethodID scan_qrcode_method;
+  static jmethodID select_task_file_method;
   static jmethodID share_file_method;
   static jmethodID detect_keyboard_method;
   static LK8000Activity *activity_instance;
@@ -58,6 +59,7 @@ public:
   void PermissionGranted();
 
   void ScanQRCode();
+  void SelectTaskFile();
 
   void ShareFile(const char *path);
 
