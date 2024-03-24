2945188c408d97ed391757e16158403792b78be2
Bruno de Lacheisserie <bruno.de.lacheisserie@gmail.com>
2024-03-24 12:12:52 +0100

    kobo rootfs : bump expat to 2.6.2

--- Scripts/kobo-build-rootfs/scripts/expat.sh
+++ Scripts/kobo-build-rootfs/scripts/expat.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e -u
-ARCHIVE_URL=https://freefr.dl.sourceforge.net/project/expat/expat/2.5.0/expat-2.5.0.tar.xz
-ARCHIVE=expat-2.5.0.tar.xz
-ARCHIVEDIR=expat-2.5.0
+
+ARCHIVE_URL=https://freefr.dl.sourceforge.net/project/expat/expat/2.6.2/expat-2.6.2.tar.xz
+ARCHIVE=expat-2.6.2.tar.xz
+ARCHIVEDIR=expat-2.6.2
 . $KOBO_SCRIPT_DIR/build-common.sh
 
 pushd $ARCHIVEDIR
